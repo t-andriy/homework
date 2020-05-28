1ceef0330baab516276cdf1ecc14b3ff6b32ce1c
t-andriy <mitere@protonmail.com>
2020-05-28 14:40:47 +0300

    Update main_05_21_2020.js

--- HW2/main_05_21_2020.js
+++ HW2/main_05_21_2020.js
@@ -1,3 +1,5 @@
+// task 1**************
+
 let x = 1;
 let y = 2;
 
@@ -14,3 +16,51 @@ console.log(typeof res2); // "string"
 let res3 = !! (x + y); // Допишіть код, необхідно використовувати змінні x і y
 console.log(res3); // true
 console.log(typeof res3); // "boolean"
+
+
+let res4 = parseFloat(Boolean(x));  // Допишіть код, необхідно використовувати змінні x і y
+console.log(res4); // NaN
+console.log(typeof res4); // "number"
+
+
+// task 2**************
+
+let a = prompt('input digital');
+if (a % 7) { 
+    console.log('fals');
+}
+else {
+    console.log('true');
+}
+
+if (a % 2) {
+    console.log('fals');
+}
+else {
+    console.log('true');
+}
+
+// task 4***************
+
+let cities = ["Rome", "Lviv", "Warsaw"];
+console.log(cities);
+
+let myArr = [cities.join('*')]; 
+console.log(myArr);
+
+
+
+// task 6************
+
+let a = prompt("input a length firs side");
+let b = prompt("input a length second side");
+let c = prompt("input a length third side");
+
+if (a > 0 && b > 0 && c > 0){
+    let halfPerimetrTriangle = p = (a + b + c)/2;
+    let areaTriangle = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    console.log(areaTriangle);
+}
+else {
+    alert("Incoerrect data!");
+}
