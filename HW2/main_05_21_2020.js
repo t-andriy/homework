e7873d473f3418d0cc4f266df61539d6a6254f18
t-andriy <mitere@protonmail.com>
2020-05-28 10:42:12 +0300

    Initial commit

---
+++ HW2/main_05_21_2020.js
@@ -0,0 +1,16 @@
+let x = 1;
+let y = 2;
+
+let res1 = x + '' + y;// Допишіть код, необхідно використовувати змінні x і y
+console.log(res1); // "12"
+console.log(typeof res1); // "string"
+
+
+let res2 = true + '' + y;  // Допишіть код, необхідно використовувати змінні x і y
+console.log(res2); // "true2"
+console.log(typeof res2); // "string"
+
+
+let res3 = !! (x + y); // Допишіть код, необхідно використовувати змінні x і y
+console.log(res3); // true
+console.log(typeof res3); // "boolean"
---
+++ HW3/main_05_25_2020.js
@@ -0,0 +1,15 @@
+// function multipli(a, b, c, d) {
+//     let result = a * b * c * d;
+//     console.log(result);
+// }
+
+// multipli(2, 3, 4, 5);
+
+let arr = [2, 3, 4, 5];
+let multipli = 1 
+for(let i = 0; i < arr.length; i++) { 
+    multipli *= arr[i];
+    
+}
+
+console.log("sum" + multipli);
