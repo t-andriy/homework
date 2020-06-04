c8b57a18225e9d65ef076901a3ba59ce45a7aa1e
t-andriy <mitere@protonmail.com>
2020-06-04 13:38:27 +0300

    Create main_06_01_2020.js

---
+++ HW5/main_06_01_2020.js
@@ -0,0 +1,25 @@
+// **********task 1****************
+
+// propsCount(currentObject),
+
+// let mentor = { 
+//     course: "JS fundamental", 
+//     duration: 3,
+//     direction: "web-development" 
+// };
+
+
+// propsCount(mentor);
+
+
+// *********task 2***************
+
+let moviHomeLand = {
+    releases: 2011,
+    episodes: 96,
+    actors: ["Claire Danes", "Damian Lewis", "Mandy Patinkin"],
+    format: "HDTV",
+    language: "English",                       
+    
+}
+console.log(Object.keys(moviHomeLand));
